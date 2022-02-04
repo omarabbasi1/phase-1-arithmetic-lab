a75705f1b5d2b44518cead335f7a5ea0a08d9c85
omarabbasi <omarkabbasi@outlook.com>
2022-02-03 18:11:41 -0800

    all test

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+
+function times() {
+    return multiply
+}
+
+
+
+const max = Math.max( 10, 8, 7, 5, 20 )
+
+
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+function avg() {
+    return mod
+}
